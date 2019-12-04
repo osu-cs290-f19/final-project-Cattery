43a502fc7fc7febb43305cae85f0b2c125aefca0
Anneliese Doerfler <anneliesedoerfler@gmail.com>
2019-12-04 14:47:41 -0800

    updated functions

--- public/index.js
+++ public/index.js
@@ -1,5 +1,21 @@
 
 function feedCat(which_cat){
-  var hungry_cat = document.getElementsByClassName("CatCard")[which_cat].getAttribute("feed-stat");
+  var hungry_cat = document.getElementById("which_cat").getAttribute("data-feed-stat");
+  if(hungry_cat < 2){
   hungry_cat++;
+}
+}
+
+function groomCat(which_cat){
+  var dirty_cat = document.getElementById("which_cat").getAttribute("data-groom-stat");
+  if(dirty_cat < 2){
+  dirty_cat++;
+}
+}
+
+function playCat(which_cat){
+  var bored_cat = document.getElementById("which_cat").getAttribute("data-play-stat");
+  if(bored_cat < 2){
+  bored_cat++;
+}
 }
