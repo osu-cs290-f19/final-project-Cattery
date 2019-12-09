c0c67ba6ff7112b016a6ee8818d056f566c85732
Anneliese Doerfler <anneliesedoerfler@gmail.com>
2019-12-08 20:57:23 -0800

    fixed color data-att

--- public/index.js
+++ public/index.js
@@ -5,7 +5,6 @@ var focus_variable = 0;
 var cat_tracker = 0;
 var catCards = document.getElementsByClassName('cat-card');
 var images = document.getElementsByClassName('catImg');
-console.log(images);
 
 //Ancestor cat
 catCards[0].addEventListener('click',function(event) {
@@ -32,11 +31,13 @@ function createCatCard(){
   var catHTML = Handlebars.templates.catCard({
     catID: "cat" + cat_tracker,
     catNUM: cat_tracker,
-    catImgID: "catImg" + cat_tracker,
     age: 0,
     photoURL: imgURL,
     color: img_num,
-    total: 0,
+    feedStat: 0,
+    groomStat: 0,
+    playStat: 0,
+    total: 0
   });
 
   var cats = document.getElementById('cats');
@@ -49,12 +50,12 @@ feed.addEventListener('click',function(){
   var hungry_cat = document.getElementById("cat" + focus_variable).getAttribute("data-feed-stat");
   var total = document.getElementById("cat" + focus_variable).getAttribute("data-total");
   var age = document.getElementById("cat" + focus_variable).getAttribute("data-is-adult");
-  console.log(total);
   if(hungry_cat < 2){
   hungry_cat++;
   document.getElementById("cat"+focus_variable).setAttribute('data-feed-stat', hungry_cat);
   total++;
   document.getElementById("cat"+focus_variable).setAttribute('data-total', total);
+  console.log(total);
 }
   if(total == 6 && age == 1){
     createCatCard();
@@ -63,8 +64,10 @@ feed.addEventListener('click',function(){
   }
   if(total == 6 && age == 0){
     document.getElementById("cat"+focus_variable).setAttribute('data-is-adult', "1");
-    var color = document.getElementById("cat" + focus_variable).getAttribute("color");
-    images[focus_variable].src = adultImgURL[focus_variable];
+    var color = document.getElementById("cat" + focus_variable).getAttribute("data-color");
+    console.log(focus_variable);
+    console.log(color);
+    images[focus_variable].src = adultImgURL[color];
     alert("All grown up!");
   }
 });
@@ -79,6 +82,7 @@ groom.addEventListener('click',function (){
   document.getElementById("cat"+focus_variable).setAttribute('data-groom-stat',dirty_cat)
   total++;
   document.getElementById("cat"+focus_variable).setAttribute('data-total', total)
+  console.log(total);
 }
   if(total == 6 && age == 1){
     createCatCard();
@@ -86,8 +90,9 @@ groom.addEventListener('click',function (){
   }
   if(total == 6 && age == 0){
     document.getElementById("cat"+focus_variable).setAttribute('data-is-adult', 1);
-    var color = document.getElementById("cat"+focus_variable).getAttribute("color");
-    images[focus_variable].src = adultImgURL[focus_variable];
+    var color = document.getElementById("cat"+focus_variable).getAttribute("data-color");
+    console.log(color);
+    images[focus_variable].src = adultImgURL[color];
     alert("All grown up!");
   }
 });
@@ -102,6 +107,7 @@ play.addEventListener('click', function(){
   document.getElementById("cat"+focus_variable).setAttribute('data-play-stat', bored_cat)
   total++;
   document.getElementById("cat"+focus_variable).setAttribute('data-total',total);
+  console.log(total);
 }
   if(total == 6 && age == 1){
     createCatCard();
@@ -110,8 +116,10 @@ play.addEventListener('click', function(){
   }
   if(total == 6 && age == 0){
     document.getElementById("cat" + focus_variable).setAttribute('data-is-adult', 1);
-    var color = document.getElementById("cat"+focus_variable).getAttribute("color");
-    images[focus_variable].src = adultImgURL[focus_variable];
+    var color = document.getElementById("cat"+focus_variable).getAttribute("data-color");
+    console.log(color);
+    console.log(focus_variable);
+    images[focus_variable].src = adultImgURL[color];
     alert("All grown up!");
   }
 });
