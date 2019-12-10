9dcd961b6940821f9c935f7da1697097082b0cd3
Alexa Pack <packa@oregonstate.edu>
2019-12-09 16:00:07 -0800

    Update

--- public/catCardTemplate.js
+++ public/catCardTemplate.js
@@ -19,16 +19,16 @@ templates['catCard'] = template({"compiler":[8,">= 4.3.0"],"main":function(conta
     + alias4(((helper = (helper = helpers.age || (depth0 != null ? depth0.age : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"age","hash":{},"data":data,"loc":{"start":{"line":1,"column":203},"end":{"line":1,"column":210}}}) : helper)))
     + "\" data-color = \""
     + alias4(((helper = (helper = helpers.color || (depth0 != null ? depth0.color : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"color","hash":{},"data":data,"loc":{"start":{"line":1,"column":226},"end":{"line":1,"column":235}}}) : helper)))
-    + "\">\n  <div class=\"cat-name-holder\">\n    <h2 class=\"cat-name\">"
+    + "\">\r\n  <div class=\"cat-name-holder\">\r\n    <h2 class=\"cat-name\">"
     + alias4(((helper = (helper = helpers.catName || (depth0 != null ? depth0.catName : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"catName","hash":{},"data":data,"loc":{"start":{"line":3,"column":25},"end":{"line":3,"column":36}}}) : helper)))
-    + "</h2>\n  </div>\n  <div class=\"cat-image-holder\">\n    <img class = \"catImg\" src= \""
+    + "</h2>\r\n  </div>\r\n  <div class=\"cat-image-holder\">\r\n    <img class = \"catImg\" src= \""
     + alias4(((helper = (helper = helpers.photoURL || (depth0 != null ? depth0.photoURL : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"photoURL","hash":{},"data":data,"loc":{"start":{"line":6,"column":32},"end":{"line":6,"column":44}}}) : helper)))
-    + "\" alt=\"Cat Image\">\n  </div>\n  \n  <div class=\"cat-stats-holder\">\n    <div class=\"groom-stat stat-holder\">\n      <img src=\"images/brush.jpg\" alt=\"Groom Icon\" class=\"stat-icon\">\n      <p class=\"groom-stat-text\">"
+    + "\" alt=\"Cat Image\">\r\n  </div>\r\n  \r\n  <div class=\"cat-stats-holder\">\r\n    <div class=\"groom-stat stat-holder\">\r\n      <img src=\"images/brush.jpg\" alt=\"Groom Icon\" class=\"stat-icon\">\r\n      <p class=\"groom-stat-text\">"
     + alias4(((helper = (helper = helpers.groomStat || (depth0 != null ? depth0.groomStat : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"groomStat","hash":{},"data":data,"loc":{"start":{"line":12,"column":33},"end":{"line":12,"column":46}}}) : helper)))
-    + "/2</p>\n    </div>\n\n    <div class=\"feed-stat stat-holder\">\n      <img src=\"images/bowl.jpg\" alt=\"Feed Icon\" class=\"stat-icon\">\n      <p class=\"feed-stat-text\"> "
+    + "/2</p>\r\n    </div>\r\n\r\n    <div class=\"feed-stat stat-holder\">\r\n      <img src=\"images/bowl.jpg\" alt=\"Feed Icon\" class=\"stat-icon\">\r\n      <p class=\"feed-stat-text\"> "
     + alias4(((helper = (helper = helpers.feedStat || (depth0 != null ? depth0.feedStat : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"feedStat","hash":{},"data":data,"loc":{"start":{"line":17,"column":33},"end":{"line":17,"column":45}}}) : helper)))
-    + "/2</p>\n    </div>\n\n    <div class=\"play-stat stat-holder\">\n      <img src=\"images/cat_toy.jpg\" alt=\"Play Icon\" class=\"stat-icon\">\n      <p class=\"play-stat-text\"> "
+    + "/2</p>\r\n    </div>\r\n\r\n    <div class=\"play-stat stat-holder\">\r\n      <img src=\"images/cat_toy.jpg\" alt=\"Play Icon\" class=\"stat-icon\">\r\n      <p class=\"play-stat-text\"> "
     + alias4(((helper = (helper = helpers.playStat || (depth0 != null ? depth0.playStat : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"playStat","hash":{},"data":data,"loc":{"start":{"line":22,"column":33},"end":{"line":22,"column":45}}}) : helper)))
-    + "/2</p>\n    </div>\n  </div>\n</div>\n";
+    + "/2</p>\r\n    </div>\r\n  </div>\r\n</div>\r\n";
 },"useData":true});
 })();
